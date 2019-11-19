f67b2dc17b4128c0f3d5ea1b677389a907ac65a9
Prathyushkotla96 <33094244+Prathyushkotla96@users.noreply.github.com>
2019-11-18 19:54:57 -0600

    Updates

---
+++ precache-manifest.42d7fcd577bdc7a0edf16142f44ddb33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f8188b5c7752da55579ea6349bb29a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2424cf409834eeb0a40",
+    "url": "/monsters-rolodex/static/css/main.d9279280.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "b2424cf409834eeb0a40",
+    "url": "/monsters-rolodex/static/js/main.fe1bf0fa.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
